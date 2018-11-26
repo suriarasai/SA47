ecdaa7b56b857876403dc1b03619fb50ff1c1ef2
suriarasai <suria.r.asai@gmail.com>
2018-11-26 14:54:18 +0800

    Third Commit

---
+++ YourFirstJavaProject/src/EmpMgrTest.java
@@ -0,0 +1,13 @@
+
+public class EmpMgrTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee e1 = new Employee(1, "dilbert", "pathetic programmer");
+		Employee e2 = new Employee(2, "alice", "another alien programmer");
+		Manager m1 = new Manager(3, "pointy", "god sent manager", 1, "moron");
+		
+
+	}
+
+}
