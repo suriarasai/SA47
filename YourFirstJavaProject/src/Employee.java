20e41bdaafda8e009eb1267fa15044538b2025f7
suriarasai <suria.r.asai@gmail.com>
2018-11-26 14:54:01 +0800

    Second Commit

--- YourFirstJavaProject/src/Employee.java
+++ YourFirstJavaProject/src/Employee.java
@@ -4,6 +4,31 @@ public class Employee {
 	private int id;
 	private String name;
 	private String designation;
+	public Employee(int id, String name, String designation) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.designation = designation;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	
 	
 	
 
--- YourFirstJavaProject/src/Manager.java
+++ YourFirstJavaProject/src/Manager.java
@@ -1,7 +1,29 @@
 
-public class Manager {
+public class Manager extends Employee {
 	
+	public Manager(int id, String name, String designation, int mid, String nickname) {
+		super(id, name, designation);
+		this.mid = mid;
+		this.nickname = nickname;
+		// TODO Auto-generated constructor stub
+	}
 	private int mid;
 	private String nickname;
+	public int getMid() {
+		return mid;
+	}
+	public void setMid(int mid) {
+		this.mid = mid;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	
+	
+	
+	
 
 }
