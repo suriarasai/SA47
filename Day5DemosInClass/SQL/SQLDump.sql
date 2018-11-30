4eea9d1c69e937e304f4e2e83199a97d92e280dc
suriarasai <suria.r.asai@gmail.com>
2018-11-30 10:10:49 +0800

    JDBC

---
+++ Day5DemosInClass/SQL/SQLDump.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `sa47`.`student` (
+  `id` INT NOT NULL,
+  `name` VARCHAR(45) NULL,
+  `naick` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
+  
+INSERT INTO `sa47`.`student` (`id`, `name`, `naick`) VALUES ('1', 'ONG HONG DAH', 'Money');
+INSERT INTO `sa47`.`student` (`id`, `name`, `naick`) VALUES ('2', 'PARINITA CHOWDHARY', 'Summary Girl');
+INSERT INTO `sa47`.`student` (`id`, `name`, `naick`) VALUES ('3', 'MAY THAZIN SOE', 'Pleasant');
---
+++ Day5DemosInClass/src/student/StudentNameComparator.java
@@ -0,0 +1,12 @@
+package student;
+
+import java.util.Comparator;
+
+public class StudentNameComparator implements Comparator<Student> {
+	@Override
+	public int compare(Student o1, Student o2) {
+		// TODO Auto-generated method stub
+		return o1.getName().compareTo(o2.getName());
+	}
+
+}
